c01b1d3fafb6b128b20f9b0d0e2a7063db1305a3
Shinjini2002 <89701012+Shinjini2002@users.noreply.github.com>
2022-10-17 20:32:17 +0530

    Create kth-smallest-element.cpp

---
+++ kth-smallest-element.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        int min=0;
+        vector<int> v;
+        for(int i=0;i<r+1;i++){
+            v.push_back(arr[i]);
+        }
+        sort(v.begin(),v.end());
+        min=v[k-1];
+        return min;
+    }
+};
+
+
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
